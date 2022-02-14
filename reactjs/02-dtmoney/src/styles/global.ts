67cd3c70a95c437156b7eedefabffe2617dbe288
André "Dezzy" Victor <hi@andrevictor.me>
2022-02-13 23:06:50 -0300

    feat: Begin ReactJS Chapter II

---
+++ reactjs/02-dtmoney/src/styles/global.ts
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #f0f2f5;
+    --shape: #fff;
+
+    --red: #e52e4d;
+    --green: #33cc95;
+    --blue: #5429cc;
+    --blue-light: #6933ff;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (min-width: 1080px) {
+      font-size: 93.75%;
+    }
+    
+    @media (min-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background-color: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursos: not-allowed;
+  }
+`;
