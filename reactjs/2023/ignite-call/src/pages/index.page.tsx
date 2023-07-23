49935fafa5184a3ca333422b1f3b7b835ad3cc6d
André Dezzy Victor <hello@andredezzy.com>
2023-07-23 19:50:36 -0300

    Remove unnecessary code and import from Home page

--- reactjs/2023/ignite-call/src/pages/index.page.tsx
+++ reactjs/2023/ignite-call/src/pages/index.page.tsx
@@ -1,9 +1,1 @@
-import { Heading } from '@ignite-ui/react'
-
-export default function Home() {
-  return (
-    <main>
-      <Heading as="h1">Test</Heading>
-    </main>
-  )
-}
+export { default } from './home'
